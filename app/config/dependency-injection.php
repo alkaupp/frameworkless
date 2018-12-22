428b9e90cceb1a2d8b1e001ded2831959205879c
Aleksi Kauppila <aleksi.kauppila@gmail.com>
2018-12-22 22:55:38 +0200

    Add dependency injection

---
+++ app/config/dependency-injection.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+use function DI\create;
+use function DI\get;
+
+return [
+    \League\Route\Strategy\ApplicationStrategy::class => create(\League\Route\Strategy\ApplicationStrategy::class),
+    \League\Route\Router::class => create(\League\Route\Router::class)
+        ->method("setStrategy", get(\League\Route\Strategy\ApplicationStrategy::class)),
+    \Nyholm\Psr7Server\ServerRequestCreatorInterface::class => create(\Nyholm\Psr7Server\ServerRequestCreator::class)
+        ->constructor(
+            new \Nyholm\Psr7\Factory\Psr17Factory(),
+            new \Nyholm\Psr7\Factory\Psr17Factory(),
+            new \Nyholm\Psr7\Factory\Psr17Factory(),
+            new \Nyholm\Psr7\Factory\Psr17Factory()
+        )
+];
--- app/index.php
+++ app/index.php
@@ -1,28 +1,29 @@
 <?php
 
-use League\Route\Http\Exception\NotFoundException;
+use DI\ContainerBuilder;
 use League\Route\Router;
 use Fwless\Controller\ExampleAction;
 use Fwless\Controller\ParameterExampleAction;
 use Fwless\Server\RequestSender;
-use Nyholm\Psr7\Factory\Psr17Factory;
 use Nyholm\Psr7\Response;
-use Nyholm\Psr7Server\ServerRequestCreator;
+use Nyholm\Psr7Server\ServerRequestCreatorInterface;
 
 require __DIR__ . "/vendor/autoload.php";
 
-$requestCreator = new ServerRequestCreator(
-    new Psr17Factory(),
-    new Psr17Factory(),
-    new Psr17Factory(),
-    new Psr17Factory()
-);
+$containerBuilder = new ContainerBuilder();
+$containerBuilder->addDefinitions(__DIR__ . "/config/dependency-injection.php");
 try {
-    $router = new Router();
+    $container = $containerBuilder->build();
+    $router = $container->get(Router::class);
     $router->get("/example", ExampleAction::class);
     $router->get("/example/{id:\d+}", ParameterExampleAction::class);
+    $requestCreator = $container->get(ServerRequestCreatorInterface::class);
     $response = $router->dispatch($requestCreator->fromGlobals());
-} catch (NotFoundException $e) {
+} catch (\League\Route\Http\Exception\NotFoundException $e) {
     $response = new Response(404, ["Content-Type" => "application/json"], json_encode(["status" => 404, "error" => $e->getMessage()]));
+} catch (\DI\NotFoundException $e) {
+    $response = new Response(500, ["Content-Type" => "application/json"], json_encode(["status" => 500, "error" => $e->getMessage()]));
+} catch (\Exception $e) {
+    $response = new Response(500, ["Content-Type" => "application/json"], json_encode(["status" => 500, "error" => $e->getMessage()]));
 }
 (new RequestSender())->send($response);
